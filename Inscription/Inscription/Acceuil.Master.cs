34c16c44f572a27278ad7cdfb9cf27583356c19d
AWhiteSheep <43411943+AWhiteSheep@users.noreply.github.com>
2018-12-14 01:21:52 -0500

    Les modals
    Modals

--- Inscription/Inscription/Acceuil.Master.cs
+++ Inscription/Inscription/Acceuil.Master.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Security;
 
 namespace Inscription
 {
@@ -11,14 +15,56 @@ namespace Inscription
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            if (this.Page.User.Identity.IsAuthenticated)
             {
-                btnConnection.Visible = false;
+                btnShowLogin.InnerText = HttpContext.Current.User.Identity.Name;
             }
             else
             {
-                btnConnection.Visible = true;
+                btnShowLogin.InnerText = "Login"; 
             }
         }
+
+        protected void ValidateUser(object sender, EventArgs e)
+        {
+            //Valider la connection de l'usager
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text.Trim();
+            int userId = 0;
+            string constr = ConfigurationManager.ConnectionStrings["ConnectionStringToDonneesActivite"].ConnectionString;
+            //Connection à la base de données
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                //Command qui appelle la procédure qui est sauvegardé côt. serveur
+                using (SqlCommand cmd = new SqlCommand("Validate_User"))
+                {
+                    //Set le type de commande
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    //Ajout des différentes valeurs
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.Parameters.AddWithValue("@Password", password);
+                    //Ajoute le para^`etre de la connection
+                    cmd.Connection = con;
+                    //ouvre
+                    con.Open();
+                    //Éxecution
+                    userId = Convert.ToInt32(cmd.ExecuteScalar());
+                    con.Close();
+                }
+                //SElon le cas
+                switch (userId)
+                {
+                    //Si retourne -1 le match est mauvais
+                    case -1:
+                        dvMessage.Visible = true;
+                        lblMessage.Text = "Username and/or password is incorrect.";
+                        break;
+                    default:                        
+                            FormsAuthentication.SetAuthCookie(username, chkRememberMe.Checked);                        
+                        break;
+                }
+            }
+        }//https://www.aspsnippets.com/Articles/Responsive-Login-Form-inside-Bootstrap-Modal-Popup-in-ASPNet.aspx
+        //Source pour la belle procédure
     }
 }
---
+++ Sql/ConnectionScripts.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE [dbo].[Validate_User]
+      @Username NVARCHAR(20),
+      @Password NVARCHAR(20)
+AS
+BEGIN
+      SET NOCOUNT ON;
+      DECLARE @UserId INT, @LastLoginDate DATETIME
+     
+      SELECT @UserId = UserId, @LastLoginDate = LastLoginDate
+      FROM Users WHERE Username = @Username AND [Password] = @Password
+     
+      IF @UserId IS NOT NULL
+      BEGIN
+            UPDATE Users
+            SET LastLoginDate = GETDATE()
+            WHERE UserId = @UserId
+            SELECT @UserId [UserId] -- User Valid
+      END
+      ELSE
+      BEGIN
+            SELECT -1 -- User invalid.
+      END
+END
+
+create table Users(
+	UserId int unique,  
+	Username nvarchar(20),
+	[Password] nvarchar(20),
+	LastLoginDate datetime 
+	primary key(Username)
+)
+
+insert into Users(UserId, Username, [Password], LastLoginDate)
+values(69696969, 'Admin', 'Admin', GETDATE())
